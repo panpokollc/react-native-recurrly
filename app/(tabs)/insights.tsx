395d2b8718ae4fb34259f20376ac33b4fce63985
Pai <charonesuk@Pais-MacBook-Pro.local>
2026-08-23 21:56:07 -0600

    replace template with actual app code

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Insights = () => {
+  return (
+    <View>
+      <Text>ComponentName</Text>
+    </View>
+  );
+};
+
+export default Insights;
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Subscriptions = () => {
+  return (
+    <View>
+      <Text>ComponentName</Text>
+    </View>
+  );
+};
+
+export default Subscriptions;
