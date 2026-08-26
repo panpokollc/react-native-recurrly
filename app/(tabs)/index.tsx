a8ec7b7f39229adf789a4b4607ac46e4c34f5699
Pai <charonesuk@Pais-MacBook-Pro.local>
2026-08-26 11:52:53 -0600

    finished home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,93 +1,101 @@
 import "@/global.css";
 import { Text, Image, View, FlatList } from "react-native";
-import { useState } from "react"
-import {Link} from "expo-router";
-import images from "@/constants/images"
-import {HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS} from "@/constants/data"
-import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context"
-import { styled } from "nativewind"
-import {icons} from "@/constants/icons"
+import { useState } from "react";
+import { Link } from "expo-router";
+import images from "@/constants/images";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "@/constants/data";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+import { icons } from "@/constants/icons";
 import { formatCurrency } from "@/lib/utils";
-import ListHeading from "@/components/ListHeading"
+import ListHeading from "@/components/ListHeading";
 import dayjs from "dayjs";
 import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
 import SubscriptionCard from "@/lib/subscriptioncard";
 const SafeAreaView = styled(RNSafeAreaView);
 
-
-
 export default function App() {
-  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<string |
-  null>(null);
+  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<
+    string | null
+  >(null);
 
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-        <View className="home-header">
-            <View className="home-user">
-                <Image
-                    source={images.avatar}
-                    className="home-avatar"
-                />
+      
+      <FlatList
+        ListHeaderComponent={() => (
+          <>
+            <View className="home-header">
+              <View className="home-user">
+                <Image source={images.avatar} className="home-avatar" />
 
                 <Text className="home-user-name">{HOME_USER.name}</Text>
+              </View>
+
+              <Image source={icons.add} className="home-add-icon" />
             </View>
 
-            <Image source={icons.add} className="home-add-icon"/>
-        </View>
+            <View className="home-balance-card">
+              <Text className="home-balance-label">Balance</Text>
 
-        <View className="home-balance-card">
-          <Text className="home-balance-label">Balance</Text>
+              <View className="home-balance-row">
+                <Text className="home-balance-amount">
+                  {formatCurrency(HOME_BALANCE.amount)}
+                </Text>
+                <Text className="home-balance-date">
+                  {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+                </Text>
+              </View>
+            </View>
 
-          <View className="home-balance-row">
-              <Text className="home-balance-amount">
-                {formatCurrency(HOME_BALANCE.amount)}
-              </Text>
-              <Text className="home-balance-date">
-                {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
+            <View className="mb-5">
+              <ListHeading title="Upcoming" />
 
-              </Text>
-          </View>
-
-
-        </View>
-
-        <View>
-            <ListHeading title="Upcoming"/>
-
-            <FlatList 
-              data={UPCOMING_SUBSCRIPTIONS} 
-
-                renderItem={({ item }) => (<UpcomingSubscriptionCard {... item}/>)}
+              <FlatList
+                data={UPCOMING_SUBSCRIPTIONS}
+                renderItem={({ item }) => (
+                  <UpcomingSubscriptionCard {...item} />
+                )}
                 keyExtractor={(item) => item.id}
                 horizontal
                 showsHorizontalScrollIndicator={false}
-                ListEmptyComponent={<Text className="home-empty-state">No upcoming
-                renewals yet.</Text>}
-            />
-        </View>
+                ListEmptyComponent={
+                  <Text className="home-empty-state">
+                    No upcoming renewals yet.
+                  </Text>
+                }
+              />
+            </View>
 
-        <View>
-            <ListHeading title="All Subscriptions"/>
-            <FlatList 
-              ListHeaderComponent={<View className="h-4"/>}
-              data={HOME_SUBSCRIPTIONS} 
-              keyExtractor={(item) => item.id}
-              renderItem={({item}) => (
-                <SubscriptionCard 
-                { ... item } 
-                expanded={expandedSubscriptionId === item.id} 
-                onPress={() => setExpandedSubscriptionId((currentId) =>
-                (currentId === item.id ? null : item.id))}
-                />
-              )}
-              extraData={expandedSubscriptionId}
-              ItemSeparatorComponent={() => <View className="h-4"/>}
-              showsVerticalScrollIndicator={false}
-              ListEmptyComponent={<Text className="home-empty-state">
-                No subscriptions yet.
-              </Text>}
-           />
-        </View>
+            <ListHeading title="All Subscriptions" />
+          </>
+        )}
+        data={HOME_SUBSCRIPTIONS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SubscriptionCard
+            {...item}
+            expanded={expandedSubscriptionId === item.id}
+            onPress={() =>
+              setExpandedSubscriptionId((currentId) =>
+                currentId === item.id ? null : item.id,
+              )
+            }
+          />
+        )}
+        extraData={expandedSubscriptionId}
+        ItemSeparatorComponent={() => <View className="h-4" />}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <Text className="home-empty-state">No subscriptions yet.</Text>
+        }
+        contentContainerClassName="pb-30" //bottom spacing
+      />
     </SafeAreaView>
   );
 }
